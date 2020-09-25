7bd2f316245020307bd77d81fe0a96da45123a6a
iliam0406 <iliam.carrion@gmail.com>
2020-09-25 19:07:33 -0300

    cambios flowmwnu

--- src/JS/Views/home.js
+++ src/JS/Views/home.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 import "../../Styles/home.css"
 import { Link } from "react-router-dom";
 
 function Home(){
+    
+    const [usermail, setUsermail] = useState([])
+
+
+    const Listuser = () =>{
+        
+    }
 
     return(
         <>
             <nav className="navbar">
-                <img  className="logo" src="/img/logo_Mesa de trabajo 1.png" alt="FlowMenu" />
+                <img  className="logo" src="/img/logo_Mesa.png" alt="FlowMenu" />
             </nav>
             <div className="encabezado">
                 <p className="titulo">Bienvenidos a Restaurante Manquehue</p>
@@ -16,8 +23,8 @@ function Home(){
             <div className="formulario">
                 <form className="order-form">
                     <p className="label">Ingrese su Correo Electrónico</p>
-                    <input className="input-text" type="email" name="" id=""/> <br/>
-                    <button className="boton-play" type="submit">ENTER</button>
+                    <input className="input-text" type="email" name="" id="" value={usermail}/> <br/>
+                    <button className="boton-play" type="submit" onClick={Listuser}>ENTER</button>
                 </form>
             </div>
         </>
